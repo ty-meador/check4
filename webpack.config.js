bc95d67e1a7b2643eaccf9d6f8d2d6be1a146a6b
Native-Coder <disturbed.meador697@gmail.com>
2021-06-07 14:40:14 -0500

    First successful node-compile with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,29 @@
 const path = require( "path" );
-const webpack = require( "webpack" );
 
-
-
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-/*
- * We've enabled TerserPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/terser-webpack-plugin
- *
- */
-
-const TerserPlugin = require( "terser-webpack-plugin" );
-
-
-
-
-module.exports = {
-	mode: "production",
+const nodeConfig = {
 	entry: "./src/Check4.js",
-	plugins: [new webpack.ProgressPlugin()],
-
-	module: {
-		rules: [{
-			test: /\.(js|jsx)$/,
-			include: [path.resolve( __dirname, "src" )],
-			loader: "babel-loader"
-		}]
-	},
-
-	optimization: {
-		minimizer: [new TerserPlugin()],
-
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
-
-			chunks: "async",
-			minChunks: 1,
-			minSize: 30000,
-			name: false
-		}
-	},
-
 	output: {
-		libraryTarget: "umd",
-		libraryExport: "default"
+		filename: "Check4.node.js",
+		path: path.resolve( __dirname, "dist" ),
+		globalObject: "this",
+		library: {
+			type: "commonjs2",
+			export: "default"
+		}
 	}
 };
+
+const webConfig = {
+	entry: "/src/Check4.js",
+	output: {
+		filename: "Check4.web.js",
+		path: path.resolve( __dirname, "dist" ),
+		globalObject: "this",
+		library: {
+			name: "Check4",
+			type: "window"
+		}
+	}
+};
+
+module.exports = [ nodeConfig, webConfig ];
